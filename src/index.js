09dfb64b19d29dd2d157d56277f3ab4e19d8ba1b
Sandro Santos <sandro.dev@gmail.com>
2020-01-24 20:15:55 -0300

    Reactotron and react-native-community/async-storage to reload only instance on reactotron app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import './config/ReactotronConfig';
 // import { Container } from './styles';
 
 export default function src() {
   return (
     <View>
+      <Text>Hello React Native!</Text>
+      <Text>Hello React Native!</Text>
+      <Text>Hello React Native!</Text>
+      <Text>Hello React Native!</Text>
       <Text>Hello React Native!</Text>
     </View>
   );

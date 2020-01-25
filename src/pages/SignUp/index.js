86f65b040af14c3b6b39a310bba4e4fdfd412dad
Sandro Santos <sandro.dev@gmail.com>
2020-01-25 15:05:08 -0300

    SignUp screen

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image } from 'react-native';
+import logo from '~/assets/logo.png';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho Login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
   );
 }

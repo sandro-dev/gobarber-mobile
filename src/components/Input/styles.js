bb1f34c8bd04b2eec25ae74f24a9deeea4d133ab
Sandro Santos <sandro.dev@gmail.com>
2020-01-25 14:53:48 -0300

    SignIn Screen

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,18 +2,21 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   padding: 0 15px;
-  height: 46px;
+  height: 58px;
   background: rgba(0, 0, 0, 0.1);
   border-radius: 4px;
 
   flex-direction: row;
   align-items: center;
+  justify-content: center;
+  margin-top: 10px;
 `;
 export const TInput = styled.TextInput.attrs({
   placeholderTextColor: 'rgba(255,255,255,0.8)',
 })`
   flex: 1;
   font-size: 15px;
-  margin-left: 10px;
+  margin-left: 5px;
+  margin-top: 10px;
   color: #fff;
 `;
